bf090af2a3b0c0fad84ee61f03eac4cc81928df2
zarab <musfiqzarab@iut-dhaka.edu>
2020-06-03 17:22:58 +0600

    finishing server setup code and add code path for well graphing

--- server/server.js
+++ server/server.js
@@ -46,6 +46,39 @@ app.get('/api', (req, res) => {
     });
 });
 
+//---------------------------path used for graphing-----------------
+
+app.get('/api/graph', (req, res) => {
+  const { metric } = req.query;
+  console.log(`Requested graph of metric: ${metric}`);
+  // 1 week time frame
+  let promises = [];
+  for (let i = 7; i >= 0; i -= 1) {
+    promises.push(getData([metric], `${i}daysAgo`, `${i}daysAgo`));
+  }
+  promises = [].concat(...promises);
+  Promise.all(promises)
+    .then((data) => {
+      // flatten list of objects into one object
+      const body = {};
+      body[metric] = [];
+      Object.values(data).forEach((value) => {
+        body[metric].push(
+          value[metric.startsWith('ga:') ? metric : `ga:${metric}`]
+        );
+      });
+      console.log(body);
+      res.send({ data: body });
+      console.log('Done');
+    })
+    .catch((err) => {
+      console.log('Error:');
+      console.log(err);
+      res.send({ status: 'Error', message: `${err}` });
+      console.log('Done');
+    });
+});
+
 // Config--------------------------------------------------------
 //setting a global constant for port
 const port = process.env.SERVER_PORT;
